3e855f07f29338ebbc0f9f6f7d6dbbcb867bd97a
Murilo Schunke <mschunke@gmail.com>
2025-09-11 16:33:16 -0300

    feat: nextjs ignore functions folder

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  // Exclude functions folder from Next.js compilation
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        "./functions": "commonjs ./functions",
+      });
+    }
+    return config;
+  },
+  // Ignore functions folder during type checking
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    dirs: ["app", "components", "lib", "types"], // Only lint these directories
+  },
 };
 
 export default nextConfig;
